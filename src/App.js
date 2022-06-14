8925f764bcd40e35d876a0e90f00842a0238b3d6
Aruzhan <aruzhan.nurmanova@nu.edu.kz>
2022-06-14 17:15:54 +0600

    gold

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import "./App.css";
+import { format } from "date-fns";
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {ReactComponent as Logo} from './assets/facebook_logo.svg'
 
 function App(props) {
   return (
     <div>
-      <div className="text">
 
+      <div className="upperSide">
+        <div className="time">{format(new Date(), "dd.MM.yyyy hh:mm")} </div>
+         
+        <div className="logo"> <Logo width={40} left={50}  />  </div>
+          <div className="search"> 
+              <input className="search-in" type="text" /> 
+          </div>
+        
+      </div>
+
+      <div className="text">
         <div className="text1">Hey</div>
         <div className="text2">Let's</div>
 
@@ -14,9 +27,7 @@ function App(props) {
 
         <div className="text5">You Can</div>
       </div>
-
     </div>
-    
   );
 }
 
